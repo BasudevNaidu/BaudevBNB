dce6354469e3704420b283fd60aeab443af8144d
Naidu Basudev <basudevnaidu2@gmail.com>
2026-03-26 00:59:20 +0530

    Replace custom logging with morgan middleware, add morgan dependency

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,16 +1,12 @@
 import express, { type Express } from "express";
 import cors from "cors";
+import morgan from "morgan";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-// Simple logging middleware
-app.use((req, res, next) => {
-  logger.info(`${req.method} ${req.url}`);
-  next();
-});
-
+app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
